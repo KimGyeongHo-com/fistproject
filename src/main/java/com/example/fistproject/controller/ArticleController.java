23aba8e16a9d6a637049717211a5d36aafff2134
KH <kkh96071@gmail.com>
2023-04-10 20:13:04 +0900

    GetMapping 을 통해 받는다. 원래는 DeleteMapping을 쓰나 지금 하는 과정에서는 지원x
    id값을 PathVariable을 통해 가져오는데 그 id를 가지고 디비에서 대상을 찾고, 찾아졌다면 repository.delete() 메소드를 가지고 삭제한다.
    
    이 때 delete라는 sql문법이 동작해서 지워짐.
    
    지워지고 난 다음에 해당 페이지로 redirect했는데, 삭제가 되었다는 것을 알려주고 싶음.
    
    redirectAttribute라는 객체 (rttr)
    
    rttr.addFlashAttribute를 통해서 적용할 수 있었다. (한번 쓰고 사라지는)

--- src/main/java/com/example/fistproject/controller/ArticleController.java
+++ src/main/java/com/example/fistproject/controller/ArticleController.java
@@ -8,10 +8,8 @@ import lombok.extern.slf4j.XSlf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -105,5 +103,21 @@ public class ArticleController {
 
         // 3: 수정 결과 페이지로 redirect
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제요청이 들어왔습니다.");
+
+        // 1: 삭제 대상을 가져옴.
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        // 2: 그 대상을 삭제
+        if(target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "delete Completed.");
+        }
+        // 3: 삭제가 완료되면 결과 페이지로 리다이렉트한다.
+        return "redirect:/articles/";
     }
 }
