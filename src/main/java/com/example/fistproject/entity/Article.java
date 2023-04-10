276af2d4a43981488c572bbc24e5c793b6fed67d
KH <kkh96071@gmail.com>
2023-04-10 14:28:55 +0900

    데이터를 조회하기 위해서는 URL을 입력해야하는데, 우리는 URL을 articles/id로 입력하기로 함. 그 입력 요청을 받기 위해선 컨트롤러의 코드 필요. URL요청을 parameter로 받아오기 위해서는 @PathVariable을 사용한다. repository에선 id값을 받아 조회할 수 있고, 없는 경우에는 orElse(null), 가져온 데이터를 등록하고(model.addAtrribute("article", articleEntity); 보여줄 페이지를 설정하여 리턴(return "articles/show"), 페이지는 resources/articles/show.mustache, header,footer 나누고, bootstrap.com에서 가져온 table 소스를 부착, #을 통해서 {{#article}}을 가지고 열고 닫고, 이 영역 안에서 article을 사용할 수 있음.

--- src/main/java/com/example/fistproject/entity/Article.java
+++ src/main/java/com/example/fistproject/entity/Article.java
@@ -1,11 +1,18 @@
 package com.example.fistproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능.
+@AllArgsConstructor
+@NoArgsConstructor// 디폴트 생성자 추가 어노테이션
+@ToString
 public class Article {
 
     @Id
@@ -15,23 +22,8 @@ public class Article {
     @Column
     private String title;
 
-
     @Column
     private String content;
 
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
